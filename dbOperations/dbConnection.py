23ebe2604803eb474cd63d96042f56d9c70723cc
Pawel <p.bednarski.pl@gmail.com>
2020-06-17 21:52:06 +0200

    herokum DB configuration

--- dbOperations/dbConnection.py
+++ dbOperations/dbConnection.py
@@ -1,14 +1,15 @@
 import psycopg2
 from psycopg2 import pool
+import os
 
 
 def connectionPool():
     try:
-        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user="postgres",
-                                                  password="mysecretpassword",
-                                                  host="127.0.0.1",
+        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user=os.environ.get('DATABASE_USER', None),
+                                                  password=os.environ.get('DATABASE_PASS', None),
+                                                  host=os.environ.get('DATABASE_HOST', None),
                                                   port="5432",
-                                                  database="postgres")
+                                                  database=os.environ.get('DATABASE_DATA', None))
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error while connecting to PostgreSQL", error)
 
