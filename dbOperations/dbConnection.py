306fea31d4f4227e89aaf82b32e319de4b56b6f7
Pawel <p.bednarski.pl@gmail.com>
2020-06-17 21:14:01 +0200

    Revert "heroku configuration"
    This reverts commit c8bf9b90

--- dbOperations/dbConnection.py
+++ dbOperations/dbConnection.py
@@ -4,14 +4,13 @@ from psycopg2 import pool
 
 def connectionPool():
     try:
-        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user="DATABASE_USER",
-                                                  password="DATABASE_PASS",
-                                                  host="DATABASE_HOST",
+        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user="postgres",
+                                                  password="mysecretpassword",
+                                                  host="127.0.0.1",
                                                   port="5432",
-                                                  database="DATABASE_DATABASE")
+                                                  database="postgres")
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error while connecting to PostgreSQL", error)
 
     else:
         return conn
-
