c8bf9b90b68e1d9fcc3211f84860b643f79081b1
Pawel <p.bednarski.pl@gmail.com>
2020-06-17 21:09:11 +0200

    heroku configuration

--- dbOperations/dbConnection.py
+++ dbOperations/dbConnection.py
@@ -4,13 +4,14 @@ from psycopg2 import pool
 
 def connectionPool():
     try:
-        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user="postgres",
-                                                  password="mysecretpassword",
-                                                  host="127.0.0.1",
+        conn = psycopg2.pool.SimpleConnectionPool(2, 50, user="DATABASE_USER",
+                                                  password="DATABASE_PASS",
+                                                  host="DATABASE_HOST",
                                                   port="5432",
-                                                  database="postgres")
+                                                  database="DATABASE_DATABASE")
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error while connecting to PostgreSQL", error)
 
     else:
         return conn
+
